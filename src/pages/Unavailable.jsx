b0df14d1ac78c2018da1b1f6a9b1a86efe9f1c2f
ChengXu Pan <chengxupan@ChengXudeMacBook-Pro.local>
2024-11-19 23:51:58 +0800

    Unavailable Page Update

--- src/pages/Unavailable.jsx
+++ src/pages/Unavailable.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Unavailable = () => {
   return (
-    <div className='fw fh df jc-c aln-itm-c' style={{height:'80vh'}}>
+    <div className='fw fh df jc-c aln-itm-c' style={{height:'60vh'}}>
         <h2>Unavailable on mobile device</h2>
         <h2>Please visit the Creamos using computer device</h2>
     </div>
