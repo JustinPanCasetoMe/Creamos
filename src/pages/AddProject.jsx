01a2430b81145beb671473a0503c0c4477fb692a
ChengXu Pan <chengxupan@ChengXudeMacBook-Pro.local>
2024-11-22 20:48:11 +0800

    Add Project Page: Radio Button Event

--- src/pages/AddProject.jsx
+++ src/pages/AddProject.jsx
@@ -5,92 +5,148 @@ import languageList from '../datas/language.json'
 
 const AddProject = () => {
 
+    const recruitMember = 1
+
     const [recruitMemeberRow, setRecruitMemberRow] = useState(1)
 
+    // State to track whether the "Paid" radio button is selected for each row
+    const [radioStatus, setRadioStatus] = useState(
+        // Initially all rows have "Unpaid" selected
+        Array(recruitMemeberRow).fill(false)
+    )
+
     const recruitRowIncrease = () => {
-        setRecruitMemberRow( recruitMemeberRow + 1)
+        setRecruitMemberRow( recruitMemeberRow + 1);
+        // Add a new "Unpaid" statsu for the new row
+        setRadioStatus([...radioStatus, false]);
+    };
+
+    const handleRoleChange = (event, rowIndex) => {
+        console.log('radioStatus[',rowIndex , '] = ', radioStatus[rowIndex])
+        // Update the state of the clicked radio button (true for "Paid", false for "Unpaid")
+        const updateStatus = [...radioStatus];
+        updateStatus[rowIndex] = event.target.value === 'Paid';
+        setRadioStatus(updateStatus);
+        // console.log(updateStatus)
     }
 
     return (
         <div>
-            <h2 className='mg-b-30'>合作專案名稱</h2>
-            <div>
-                <div className='df aln-itm-c'>
-                    <h2>徵集合作夥伴</h2>
-                    <input type="text" />
+            <div className='df aln-itm-c mg-b-30'>
+                    <h2 className='mg-r-10'>合作專案名稱</h2>
+                    <input type="text" style={{height:'24px', width:'200px'}} className=''/>
                 </div>
+            <div>
+                {/* <div className='df aln-itm-c mg-b-30'>
+                    <h2 className='mg-r-10'>徵集合作夥伴</h2>
+                </div> */}
                 <div className='mg-b-30 pd-10' style={{backgroundColor:'var(--greyBG-02)'}}>
                     <table className='fw mg-b-20'>
                         <thead>
                             <tr>
-                                <th><h2>徵集夥伴角色</h2></th>
-                                <th><h2>角色種類</h2></th>
-                                <th><h2>需求人數</h2></th>
-                                <th><h2>詳細描述</h2></th>
-                                <th><h2>付費與否</h2></th>
+                                <th style={{width:'18%'}}><h2>徵集夥伴角色</h2></th>
+                                <th style={{width:'16%'}}><h2>角色種類</h2></th>
+                                <th style={{width:'18%'}}><h2>需求人數</h2></th>
+                                <th style={{width:'18%'}}><h2>詳細描述</h2></th>
+                                <th style={{width:'22%'}}><h2>付費與否</h2></th>
                             </tr>
                         </thead>
-                        {Array.from({length: recruitMemeberRow}, (_, index) => index+1).map((item, index) => {
-                            return(
-                                <tbody key={index}>
-                                    <tr>
-                                        <th style={{width:'25%'}}>
-                                            <div className='df aln-itm-c'>
-                                            <h3 className='mg-r-10'>參與角色</h3>
-                                                <select name="" id="">
-                                                    {characterList.map((character, index) => {
-                                                        return(
-                                                            <option value={character.character} key={index}>{character.character}</option>
-                                                        )
-                                                    })}
-                                                </select>
-                                            </div>
+                        <tbody>
+                            {Array.from({length: recruitMemeberRow}, (_, index) => index+1).map((item, index) => {
+                                return(
+                                    <tr key={index}>
+                                        <th>
+                                            <h4>參與角色</h4>
+                                            <select name="" id="" style={{height:'24px'}}>
+                                                {characterList.map((character, index) => {
+                                                    return(
+                                                        <option value={character.character} key={index}>{character.character}</option>
+                                                    )
+                                                })}
+                                            </select>
                                         </th>
-                                        <td>
-                                            <h3>按參與角色跳不同種類</h3>
+                                        <td className='pd-r-20'>
+                                            <h4>按參與角色跳不同種類</h4>
+                                            <select name="" id="" style={{height:'24px'}}>
+                                                {characterList.map((character, index) => {
+                                                    return(
+                                                        <option value={character.character} key={index}>{character.character}</option>
+                                                    )
+                                                })}
+                                            </select>
                                         </td>
-                                        <td>
-                                            <div className='df aln-itm-c'>
-                                            <h3 className='mg-r-10'>所需人數</h3>
-                                                <select name="" id="">
-                                                    {Array.from({length:10}, (_, index) => index+1).map((number, index) => {
-                                                        return(
-                                                            <option value={number} key={index}>{number}</option>
-                                                        )
-                                                    })}
-                                                </select>
-                                            </div>
+                                        <td className='pd-r-20'>
+                                            <h4>所需人數</h4>
+                                            <select name="" id="" style={{height:'24px'}}>
+                                                {Array.from({length:10}, (_, index) => index+1).map((number, index) => {
+                                                    return(
+                                                        <option value={number} key={index}>{number}</option>
+                                                    )
+                                                })}
+                                            </select>
                                         </td>
-                                        <td>
-                                            <textarea name="" id="" style={{resize: 'vertical'}}></textarea>
+                                        <td className='pd-r-20'>
+                                            <textarea name="" id="" style={{resize: 'vertical'}} className='fw pd-10' />
                                         </td>
-                                        <td>
-                                            <h3>付費與否</h3>
+                                        <td className='pd-r-20'>
+                                            <form action="" className='pd-r-20 df aln-itm-c mg-b-10'>
+                                                <div className='mg-r-20'>
+                                                    <input
+                                                        type="radio"
+                                                        name={`role-${index}`}
+                                                        id={`Unpaid-${index}`}
+                                                        value='Unpaid'
+                                                        className='mg-r-10'
+                                                        checked={!radioStatus[index]}
+                                                        onChange={(event) => handleRoleChange(event, index)}
+                                                    />
+                                                    <label htmlFor={`Unpaid-${index}`}>徵免費夥伴</label>
+                                                </div>
+                                                <div className='mg-r-20' >
+                                                    <input
+                                                        type="radio"
+                                                        name={`role-${index}`}
+                                                        id={`Paid-${index}`}
+                                                        value='Paid'
+                                                        className='mg-r-10'
+                                                        checked={radioStatus[index]}
+                                                        onChange={(event) => handleRoleChange(event, index)}
+                                                    />
+                                                    <label htmlFor={`Paid-${index}`}>付費給參與者</label>
+                                                </div>
+                                            </form>
+                                            { radioStatus[index] && 
+                                                <input 
+                                                    type="text"
+                                                    placeholder='填寫價格範圍'
+                                                    style={{width:'300px', height:'30px'}}
+                                                /> 
+                                            }
+                                            
                                         </td>
                                     </tr>
-                                </tbody>
-                            )
-                        })}
+                                )
+                            })}
+                        </tbody>
                     </table>
-                    <button className='pd-10' onClick={recruitRowIncrease}>+</button>
-                    {console.log("recruitMemeberRow = ", recruitMemeberRow)}
+                    <button className='pd-10' onClick={recruitRowIncrease}>+  Add new row</button>
                 </div>
 
                 {/* 選擇專案地點 */}
                 <div className='mg-b-30'>
-                    <h2>專案合作地點（可複選）</h2>
+                    <h2 className='mg-b-10'>專案合作地點（可複選）</h2>
                     <div className='df fl-wp'>
                         {locationList.map((location, index) => {
                             return(
                                 <div style={{width:'120px'}} key={index}>
                                     <input
                                         type="checkbox"
-                                        id={location.id}
+                                        id={location.location}
                                         name={location.location}
                                         value={location.location}
                                         style={{color:'#fff', backgroundColor:'#fff'}}
                                         className='mg-r-10' />
-                                    <label htmlFor={location.id} className='fw'>{location.location}</label>
+                                    <label htmlFor={location.location} className='fw'>{location.location}</label>
                                 </div>
                             )
                         })
@@ -100,19 +156,19 @@ const AddProject = () => {
 
                 {/* 選擇作品語言 */}
                 <div className='mg-b-30'>
-                    <h2>作品語言（可複選）作品尚無語言者不需勾選</h2>
+                    <h2 className='mg-b-10'>作品語言（可複選）作品尚無語言者不需勾選</h2>
                     <div className='df fl-wp'>
                         {languageList.map((language, index) => {
                             return(
                                 <div style={{width:'120px'}} key={index}>
                                     <input
                                         type="checkbox"
-                                        id={language.id}
+                                        id={language.language}
                                         name={language.language}
                                         value={language.language}
                                         style={{color:'#fff', backgroundColor:'#fff'}}
                                         className='mg-r-10' />
-                                    <label htmlFor={language.id} className='fw'>{language.language}</label>
+                                    <label htmlFor={language.language} className='fw'>{language.language}</label>
                                 </div>
                             )
                         })
@@ -122,7 +178,7 @@ const AddProject = () => {
 
                 {/* 填寫合作專案描述 */}
                 <div className='mg-b-30'>
-                    <h2>合作專案內容描述</h2>
+                    <h2 className='mg-b-10'>合作專案內容描述</h2>
                     <textarea name="" id="" className='fw pd-10' style={{height:'100px', resize:'vertical'}}></textarea>
                 </div>
 
